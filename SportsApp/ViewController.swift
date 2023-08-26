e5d882d2a88e8f2f046dd36fa3e9048e1a32696d
Kostas.Stergiannis <kostasst@expressdigibooks.com>
2023-08-26 12:46:44 +0300

    Initial Commit

---
+++ SportsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportsApp
+//
+//  Created by kwstas stergiannis on 26/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
